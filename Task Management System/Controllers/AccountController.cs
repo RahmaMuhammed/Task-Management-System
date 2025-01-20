8df3d14df1d960fdee6721ccbd058bab596fe890
RahmaMuhammed <rahmamuhammed285@gmail.com>
2025-01-20 09:31:15 +0200

    Implement Login functionality in AccountController with basic input validation and LoginViewModel

--- Task Management System/Controllers/AccountController.cs
+++ Task Management System/Controllers/AccountController.cs
@@ -8,10 +8,12 @@ namespace Task_Management_System.Controllers
     public class AccountController : Controller
     {
         private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
 
-        public AccountController(UserManager<ApplicationUser> userManager) 
+        public AccountController(UserManager<ApplicationUser> userManager,SignInManager<ApplicationUser> signInManager) 
         {
             _userManager = userManager;
+            _signInManager = signInManager;
         }
         // Register
         public IActionResult Register()
@@ -50,7 +52,42 @@ namespace Task_Management_System.Controllers
             }
                 return View(model);
         }
+
         // Login
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByEmailAsync(model.Email);
+                if (user is not null)
+                {
+                    var result = await _userManager.CheckPasswordAsync(user, model.Password);
+                    if (result)
+                    {
+                        var loginResult = await _signInManager.PasswordSignInAsync(user, model.Password, model.RememberMe, false);
+                        if (loginResult.Succeeded)
+                        {
+                            return RedirectToAction("Index", "Home");
+                        }
+                    }
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, "Password is Incorrect");
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Email is Not Exists");
+                }
+            }
+                return View(model);
+        }
         // Logout
         // Forget Password
         // Reset Password 
